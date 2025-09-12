0c2754f09a50432bf60e6288f958931a3eceb03b
Prasadchavala <prasad.chavala@vibhotech.com>
2025-09-12 17:22:37 +0530

    new changes

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const db_1 = __importDefault(require("./config/db"));
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+const http_1 = require("http");
+const user_1 = __importDefault(require("./routes/user"));
+dotenv_1.default.config();
+(0, db_1.default)();
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)({ origin: "http://localhost:5173", credentials: true }));
+app.use(express_1.default.json());
+app.use("/user", user_1.default);
+const server = (0, http_1.createServer)(app);
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
