d5bea98456365033f7733128078e4fcf4e952e99
Prasadchavala <prasad.chavala@vibhotech.com>
2025-09-12 18:24:50 +0530

    new changes

--- dist/index.js
+++ dist/index.js
@@ -12,7 +12,21 @@ const user_1 = __importDefault(require("./routes/user"));
 dotenv_1.default.config();
 (0, db_1.default)();
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)({ origin: "http://localhost:5173", credentials: true }));
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://secureportalfrontend.netlify.app",
+];
+app.use((0, cors_1.default)({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        }
+        else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true
+}));
 app.use(express_1.default.json());
 app.use("/user", user_1.default);
 const server = (0, http_1.createServer)(app);
