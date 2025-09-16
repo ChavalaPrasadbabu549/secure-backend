47cde2ebbddd064d9c1ee7261273748d23fce8df
Prasadchavala <prasad.chavala@vibhotech.com>
2025-09-16 10:56:27 +0530

    new changes

--- src/index.ts
+++ src/index.ts
@@ -32,5 +32,6 @@ app.use("/user", userRoutes);
 
 const server = createServer(app);
 
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
